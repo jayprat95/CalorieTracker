20dadd5d16211ccc8a02fed8a1f47472b566f4aa
Jayanth Prathipati <jayanth@vt.edu>
2016-02-05 13:20:37 -0500

    Initial Commit

---
+++ CalorieTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CalorieTracker
+//
+//  Created by Jayanth Prathipati on 2/5/16.
+//  Copyright © 2016 TouchTap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
