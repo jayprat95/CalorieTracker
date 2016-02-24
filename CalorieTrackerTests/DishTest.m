3f6fe588c92b3031b55db6b5ebcb73cb0c8c2183
Jayanth Prathipati <jayanth@vt.edu>
2016-02-24 13:44:57 -0500

    added tests for creating a new item and adding it to the DB

--- CalorieTrackerTests/DishTest.m
+++ CalorieTrackerTests/DishTest.m
@@ -20,7 +20,8 @@
 
 - (void)setUp {
     [super setUp];
-
+    
+    //get the calorie tracker core data model as an NSManagedObjectContext
     NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"CalorieTracker" withExtension:@"momd"];
     NSManagedObjectModel *mom = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:mom];
@@ -36,7 +37,7 @@
     [super tearDown];
 }
 
-- (void)testExample {
+- (void)testCreatingNewDish {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
     Dish *newDish = [NSEntityDescription insertNewObjectForEntityForName:@"Dish" inManagedObjectContext:self.moc];
@@ -54,11 +55,40 @@
     XCTAssertFalse(self.moc.hasChanges,"All the changes should be saved");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testAccessingCoreDataDB {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+    Dish *newDish = [NSEntityDescription insertNewObjectForEntityForName:@"Dish" inManagedObjectContext:self.moc];
+    newDish.title = @"Tomato";
+    newDish.timeStamp = [NSDate date];
+    newDish.calories = 200;
+    
+    // Save the context.
+    NSError *error = nil;
+    if (![self.moc save:&error]) {
+        // Replace this implementation with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        XCTFail(@"Error saving in \"%s\" : %@, %@", __PRETTY_FUNCTION__, error, [error userInfo]);
+    }
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    [fetchRequest setEntity:
+     [NSEntityDescription entityForName:@"Dish" inManagedObjectContext:self.moc]];
+    
+    NSArray *dishes = [self.moc
+                                       executeFetchRequest:fetchRequest error:&error];
+    Dish *dish2 = dishes[0];
+    XCTAssertEqual(@"Tomato", dish2.title, @"Should be the same title");
+    XCTAssertEqual(200, dish2.calories, @"Should be the same amount of calories");
+    
+    
 }
+
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
 
 @end
