495b1c450f9fd7a8a8cf38e19943e61dbda450d2
Julia Zhao <zjulia@vt.edu>
2016-03-20 20:43:21 -0400

    When run, shows a list of dining halls

---
+++ CalorieTracker/DiningTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  DataTableViewController.h
+//  CalorieTracker
+//
+//  Created by Julia on 3/20/16.
+//  Copyright © 2016 TouchTap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiningTableViewController : UITableViewController
+
+@end
